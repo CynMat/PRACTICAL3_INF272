380b2af6681d204a605384aa8d0d753024b8019f
Home <Home@CYNTHIA>
2025-03-16 01:28:33 +0200

    Add project files.

---
+++ S1L05_RWD/Models/Consult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace S1L05_RWD.Models
+{
+    public class Consult
+    {
+        [Display (Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display (Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display (Name = "Email Address")]
+        public string EmailAddress { get; set; }
+    }
+}
